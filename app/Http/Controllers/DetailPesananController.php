6342df09ccf138f4f49eba29fac4e27c972e1218
uywahsugab <wahyu_bagus_26rpl@student.smktelkom-mlg.sch.id>
2020-07-14 14:47:49 +0700

    DetailPesanan Done

--- app/Http/Controllers/DetailPesananController.php
+++ app/Http/Controllers/DetailPesananController.php
@@ -32,4 +32,77 @@ class DetailPesananController extends Controller
         $detail->status = $request->input('status');
         $detail->save();
     }
+
+    private function delete(Request $request)
+    {
+        $detail = DetailPesanan::where('id', $request->input('id'))->first();
+        $detail->delete();
+    }
+
+    public function index($id_pesanan)
+    {
+        $detail_pesanan = Pesanan::where('id', $id_pesanan)->with('detail_pesanan')->first();
+        return view('detail.index', compact('detail_pesanan' , 'id_pesanan'));
+    }
+
+    public function vStore($id_pesanan)
+    {
+        $barang = Barang::where('status' , 'Tersedia')->get();
+        return view('detail.store', compact('barang', 'id_pesanan'));
+    }
+
+    public function vEdit($id_pesanan, $id)
+    {
+        $detail = DetailPesanan::where('id', $id)->with('barang')->first();
+        $barang = Barang::where('status', 'Tersedia')->get();
+        return view('detail.edit', compact('detail', 'barang', 'id_pesanan'));
+    }
+
+    public function validateStore(Request $request, $id_pesanan)
+    {
+        $this->validate($request, [
+            'id_barang' => 'required',
+            'jumlah' => 'required|numeric'
+        ]);
+
+        $this->store($request, $id_pesanan);
+        return redirect()->back();
+    }
+
+    public function massInsert($id_pesanan)
+    {
+        DetailPesanan::insert(Session::get('detail'));
+        $jmlh_detail = count(Session::get('detail'));
+
+        Session::forget('detail');
+        return redirect()->route('detail.index', $id_pesanan)->with('detail_succ', 'Berhasil menambah' . $jmlh_detail . 'item detail pesanan!');
+    }
+
+    public function validateEdit(Request $request, $id, $id_pesanan)
+    {
+        $this->validate($request, [
+            'id_barang' => 'required',
+            'jumlah' => 'required',
+            'status' => 'required'
+        ]);
+
+        $this->edit($request, $id);
+        return redirect()->route('detail.index', $id_pesanan)->with('detail_succ', 'Berhasil mengubah data');
+    }
+
+    public function validateDelete(Request $request)
+    {
+        $this->validate($request, [
+            'id' =>'required'
+        ]);
+
+        $this->delete($request);
+        return redirect()->back()->with('detail_succ', 'Berhasil menghapus data');
+    }
+
+    public function getDataWithJSON($id)
+    {
+        $detail = DetailPesanan::where('id', $id)->with('barang')->first();
+        return response()->json($detail);
+    }
 }
